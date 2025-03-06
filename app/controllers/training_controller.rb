dce459e8dcf9fa9ac4cdaa087ba4843a3c8dc5b6
谷藤竜也 <bluehorse091011@yahoo.co.jp>
2025-03-06 15:06:01 +0900

    Add local changes to training controller

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -1,119 +1,122 @@
-def training_abs
-    @users = User.all
-  end
+class Training_Controller < ApplicationController
+
+    def training_abs
+      @users = User.all
+    end
+    
+    def update_abs_modal
+      @user = User.find(params[:id])
+      puts params[:user][:abs]
+      if @user.update(user_params)
+        flash[:success] = "トレーニングメニューを登録しました。"
+        redirect_to user_url(@user)  # 保存成功時のリダイレクト
+      else
+        flash[:error] = "トレーニングメニューを登録に失敗しました。"
+        render :edit  # 失敗時にエラーメッセージを表示
+      end
+    end
+
+    def training_abs_modal
+      @user = User.find(params[:id])
+    end
+
+    def training_arms
+      @users = User.all
+    end
+
+    def training_arms_modal
+      @user = User.find(params[:id])
+    end  
+
+    def update_arms_modal 
+      @user = User.find(params[:id])
+      puts params[:user][:arms]
+      if @user.update(user_params)
+        flash[:success] = "トレーニングメニューを登録しました。"
+        redirect_to user_url(@user)  # 保存成功時のリダイレクト
+      else
+        flash[:error] = "トレーニングメニューを登録に失敗しました。"
+        render :edit  # 失敗時にエラーメッセージを表示
+      end
+    end
   
-  def update_abs_modal
-    @user = User.find(params[:id])
-    puts params[:user][:abs]
-    if @user.update(user_params)
-      flash[:success] = "トレーニングメニューを登録しました。"
-      redirect_to user_url(@user)  # 保存成功時のリダイレクト
-    else
-      flash[:error] = "トレーニングメニューを登録に失敗しました。"
-      render :edit  # 失敗時にエラーメッセージを表示
+    def training_back
+      @users = User.all
+    end 
+
+    def training_back_modal
+      @user = User.find(params[:id])
+    end 
+
+    def update_back_modal
+      @user = User.find(params[:id])
+      puts params[:user][:back]
+      if @user.update(user_params)
+        flash[:success] = "トレーニングメニューを登録しました。"
+        redirect_to user_url(@user)  # 保存成功時のリダイレクト
+      else
+        flash[:error] = "トレーニングメニューを登録に失敗しました。"
+        render :edit  # 失敗時にエラーメッセージを表示
+      end
     end
-  end
 
-  def training_abs_modal
-    @user = User.find(params[:id])
-  end
+    def training_chest
+      @users = User.all
+    end 
 
-  def training_arms
-    @users = User.all
-  end
-
-  def training_arms_modal
-    @user = User.find(params[:id])
-  end  
-
-  def update_arms_modal 
-    @user = User.find(params[:id])
-    puts params[:user][:arms]
-    if @user.update(user_params)
-      flash[:success] = "トレーニングメニューを登録しました。"
-      redirect_to user_url(@user)  # 保存成功時のリダイレクト
-    else
-      flash[:error] = "トレーニングメニューを登録に失敗しました。"
-      render :edit  # 失敗時にエラーメッセージを表示
+    def training_chest_modal
+      @user = User.find(params[:id])
     end
-  end
- 
-  def training_back
-    @users = User.all
-  end 
 
-  def training_back_modal
-    @user = User.find(params[:id])
-  end 
-
-  def update_back_modal
-    @user = User.find(params[:id])
-    puts params[:user][:back]
-    if @user.update(user_params)
-      flash[:success] = "トレーニングメニューを登録しました。"
-      redirect_to user_url(@user)  # 保存成功時のリダイレクト
-    else
-      flash[:error] = "トレーニングメニューを登録に失敗しました。"
-      render :edit  # 失敗時にエラーメッセージを表示
+    def update_chest_modal
+      @user = User.find(params[:id])
+      puts params[:user][:chest]
+      if @user.update(user_params)
+        flash[:success] = "トレーニングメニューを登録しました。"
+        redirect_to user_url(@user)  # 保存成功時のリダイレクト
+      else
+        flash[:error] = "トレーニングメニューを登録に失敗しました。"
+        render :edit  # 失敗時にエラーメッセージを表示
+      end
     end
-  end
+    
+    def training_shoulder
+      @users = User.all
+    end 
 
-  def training_chest
-    @users = User.all
-  end 
-
-  def training_chest_modal
-    @user = User.find(params[:id])
-  end
-
-  def update_chest_modal
-    @user = User.find(params[:id])
-    puts params[:user][:chest]
-    if @user.update(user_params)
-      flash[:success] = "トレーニングメニューを登録しました。"
-      redirect_to user_url(@user)  # 保存成功時のリダイレクト
-    else
-      flash[:error] = "トレーニングメニューを登録に失敗しました。"
-      render :edit  # 失敗時にエラーメッセージを表示
+    def training_shoulder_modal
+      @user = User.find(params[:id])
     end
-  end
-  
-  def training_shoulder
-    @users = User.all
-  end 
 
-  def training_shoulder_modal
-    @user = User.find(params[:id])
-  end
-
-  def update_shoulder_modal
-    @user = User.find(params[:id])
-    puts params[:user][:shoulder]
-    if @user.update(user_params)
-      flash[:success] = "トレーニングメニューを登録しました。"
-      redirect_to user_url(@user)  # 保存成功時のリダイレクト
-    else
-      flash[:error] = "トレーニングメニューを登録に失敗しました。"
-      render :edit  # 失敗時にエラーメッセージを表示
+    def update_shoulder_modal
+      @user = User.find(params[:id])
+      puts params[:user][:shoulder]
+      if @user.update(user_params)
+        flash[:success] = "トレーニングメニューを登録しました。"
+        redirect_to user_url(@user)  # 保存成功時のリダイレクト
+      else
+        flash[:error] = "トレーニングメニューを登録に失敗しました。"
+        render :edit  # 失敗時にエラーメッセージを表示
+      end
     end
-  end
 
-  def training_thighs
-    @users = User.all
-  end 
+    def training_thighs
+      @users = User.all
+    end 
 
-  def training_thighs_modal
-    @user = User.find(params[:id])
-  end
-
-  def update_thighs_modal
-    @user = User.find(params[:id])
-    puts params[:user][:thighs]
-    if @user.update(user_params)
-      flash[:success] = "トレーニングメニューを登録しました。"
-      redirect_to user_url(@user)  # 保存成功時のリダイレクト
-    else
-      flash[:error] = "トレーニングメニューを登録に失敗しました。"
-      render :edit  # 失敗時にエラーメッセージを表示
+    def training_thighs_modal
+      @user = User.find(params[:id])
     end
-  end
+
+    def update_thighs_modal
+      @user = User.find(params[:id])
+      puts params[:user][:thighs]
+      if @user.update(user_params)
+        flash[:success] = "トレーニングメニューを登録しました。"
+        redirect_to user_url(@user)  # 保存成功時のリダイレクト
+      else
+        flash[:error] = "トレーニングメニューを登録に失敗しました。"
+        render :edit  # 失敗時にエラーメッセージを表示
+      end
+    end  
+end
